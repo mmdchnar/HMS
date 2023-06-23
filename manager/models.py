e17002d6dccae932eefa4f298801e9e6fb1ce42a
Mohammad Chenarani <115184760+mmdchnar@users.noreply.github.com>
2023-06-23 16:11:21 +0330

    fix missing import validations error

--- manager/models.py
+++ manager/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.core.validators import RegexValidator
+from django.core.validators import RegexValidator, ValidationError
+from django.utils.translation import gettext_lazy as _
 
 
 class CustomUser(User):
